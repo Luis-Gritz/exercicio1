6238661f502860069cf63e644c9a977c657a94a6
luis <luis.gritz86@gmail.com>
2019-08-21 20:30:21 -0300

    Exercicio1

---
+++ app/src/main/java/com/example/exercicio1/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.exercicio1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editNota1;
+    private EditText editNota2;
+    private EditText editNota3;
+    private CheckBox checkMedia1;
+    private TextView textDivisao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editNota1 = findViewById(R.id.editNota1);
+        editNota2 = findViewById(R.id.editNota2);
+        editNota3 = findViewById(R.id.editNota3);
+        checkMedia1 = findViewById(R.id.checkMedia1);
+        textDivisao = findViewById(R.id.textDivisao);
+    }
+
+    public void calcula_media(View view) {
+
+        String valorNotaUm = editNota1.getText().toString();
+        String valorNotaDois = editNota2.getText().toString();
+        String valorNotaTres = editNota3.getText().toString();
+        double notaUm = Double.parseDouble(valorNotaUm);
+        double notaDois = Double.parseDouble(valorNotaDois);
+        double notaTres = Double.parseDouble(valorNotaTres);
+        double media;
+
+        if (checkMedia1.isChecked() == true){
+
+            double a = notaUm * 1;
+            double b = notaDois * 2;
+            double c = notaTres * 3;
+            media =(a+b+c)/6;
+
+        }else{
+
+            double somaNotas = notaUm + notaDois + notaTres;
+            media = somaNotas/3;
+
+        }
+
+        DecimalFormat df = new DecimalFormat("#0.00");
+
+        String formatado = "Nota: " + df.format(media);
+
+        textDivisao.setText(formatado);
+    }
+}
